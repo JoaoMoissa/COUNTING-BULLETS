6dac2ea901df2f458a7dff476067d32f33d076db
João Vitor Moissa <joaomoissapersonal@Gmail.com>
2026-07-23 07:28:39 -0300

    better movement
    Now you can't run backwards or when strafing, only when going forward or diagonally forward.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+var can_sprint = true
 var speed
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
@@ -37,16 +38,19 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+	
+	# Defines the input direction.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	
 		
 	# Handle Sprint.
-	if Input.is_action_pressed("sprint"):
+	if Input.is_action_pressed("sprint") and input_dir.y < 0:
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
+		
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
@@ -58,6 +62,9 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+		
+		
+		
 		
 	# Head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
